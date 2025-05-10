9534f02f6d33cdaeb9fb196f93c3856ea7acd57b
DWTD <zhora.mosa@mail.ru>
2025-05-10 13:00:56 +0300

    Laracast episode 06: View Data and Route Wildcards

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,14 +7,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
-<!--
-This example requires updating your template:
-
-```
-<html class="h-full bg-gray-100">
-<body class="h-full">
-```
--->
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -27,7 +19,7 @@ This example requires updating your template:
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                             <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                             <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
