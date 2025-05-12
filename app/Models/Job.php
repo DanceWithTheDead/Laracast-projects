64ba7b4b3d72e1c11103b6982d2e6aa76daa3ccb
DanceWithTheDead <zhora.mosa@mail.ru>
2025-05-12 17:49:18 +0300

    Laracast episode 10: Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings'; //Привязка к таблице по названию,т.к у модели и таблицы разные имена.
 
     protected $fillable = ['title', 'salary']; //Поля которые можно заполнять.
