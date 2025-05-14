acf1099bcbb4c3bc2f6d5a9a4a6559ce79bd86b2
DWTD <zhora.mosa@mail.ru>
2025-05-14 16:54:34 +0300

    Laracast episode 19: Routes Reloaded - 6 Essential Tips

--- routes/web.php
+++ routes/web.php
@@ -1,93 +1,16 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::get('/', function () {
-    return view('home');
-});
-
-//Index
-Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->latest()->simplePaginate(5);
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Create
-Route::get('/jobs/create', function (){
-    return view('jobs.create');
-});
-
-//Show
-Route::get('/jobs/{id}', function ($id)  {
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job'=> $job]);
-});
-
-//Store
-Route::post('/jobs', function (){
-    request()->validate([
-       'title' => ['required','min:3'],
-       'salary' => ['required']
-    ]);
-
-    Job::create([
-       'title' => request('title'),
-       'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-    return redirect('/jobs');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 
-
-//Edit
-Route::get('/jobs/{id}/edit', function ($id)  {
-    $job = Job::find($id);
-
-    return view('jobs.edit', ['job'=> $job]);
-});
-
-//Update
-Route::patch('/jobs/{id}', function ($id)  {
-
-    request()->validate([
-        'title' => ['required','min:3'],
-        'salary' => ['required']
-    ]);
-    //authorize (On hold...)
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
+Route::resource('jobs', JobController::class);
 
 
-});
-
-//Destroy
-Route::delete('/jobs/{id}', function ($id)  {
-    //authorize (On hold...)
-
-    Job::findOrFail($id)->delete();
-
-
-    return redirect('/jobs');
-
-
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
 
