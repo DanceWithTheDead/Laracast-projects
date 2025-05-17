a9601b57e9de32e8d0bdbc07bf9725582fdfb206
DWTD <zhora.mosa@mail.ru>
2025-05-17 17:16:19 +0300

    Laracast episode 26: Get Your Build Process in Order

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,7 +7,7 @@
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-300 rounded-lg">
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
                 <div>
-                    <h1><strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary']}} per year</h1>
+                    <h1><strong class="text-laracasts">{{ $job['title'] }}:</strong> Pays {{ $job['salary']}} per year</h1>
                 </div>
             </a>
         @endforeach
